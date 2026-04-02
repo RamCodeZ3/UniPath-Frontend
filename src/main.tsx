521458aec832ccf01f0eaa954f317dd37e72a876
yerepf <yeremyyaelpujolsfelix@gmail.com>
2026-04-02 14:39:42 -0400

    feat: add primereact

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from './routes';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import './index.css';
 
 const router = createBrowserRouter(routes);
