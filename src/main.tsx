7b13134af3c390f26ef2161d633924e0cd3a46f0
Rusber-B <rusber.batista6@gmail.com>
2026-04-02 20:48:40 -0400

    feat: update routes with protected and auth routes, add Redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import routes from './routes';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import routes from './routes.tsx';
 import './index.css';
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-        <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 );
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,31 @@
-import Login from "./pages/login/Login";
+import Login from './pages/login/Login';
+import AuthCallback from './pages/auth-callback/AuthCallback';
+import ConfirmEmail from './pages/confirm-email/ConfirmEmail';
+import Dashboard from './pages/dashboard/Dashboard';
+import ProtectedRoute from './shared/components/ProtectedRoute';
 
 const routes = [
-    {
-        path: "/",
-        element: <Login/>
-    }
+  {
+    path: '/login',
+    element: <Login />,
+  },
+
+  {
+    path: '/auth/callback',
+    element: <AuthCallback />,
+  },
+  {
+    path: '/confirm-email',
+    element: <ConfirmEmail />,
+  },
+  {
+    path: '/dashboard',
+    element: (
+      <ProtectedRoute>
+      <Dashboard />{/* tu página principal */}
+      </ProtectedRoute>
+    ),
+  },
 ];
 
 export default routes;
